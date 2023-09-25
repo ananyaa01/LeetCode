2e642bd371380016f0f3353f3e227f1cd779e942
Ananya Agarwal <80957726+ananyaa01@users.noreply.github.com>
2023-09-25 09:45:22 +0530

    Time: 40 ms (44.17%), Space: 16.3 MB (53.02%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        ans = []
+        for i, c in enumerate(command):
+            if c == 'G':
+                ans.append(c)
+            elif c == '(':
+                ans.append('o' if command[i + 1] == ')' else 'al')
+        return ''.join(ans)
+        
